91de8ac3a0dcd7447afe395d7fa420e4d158200e
ifer-itcast <yangkangkang@itcast.cn>
2020-08-17 00:25:37 +0800

    loading和错误处理

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -4,12 +4,30 @@ import { fetchUser } from './store/actionCreators';
 
 class User extends Component {
     render() {
+        const { isFetching, error, user } = this.props.user;
+        let data = null;
+        if(error) {
+            data = error;
+        } else if(isFetching) {
+            data = "Loading...";
+        } else {
+            data = user && <ul>{
+                user.jokes.map((item,index) => <li key={index}>{item}</li>)
+            }</ul>;
+        }
         return (
             <div>
+                <div>{data}</div>
                 <button onClick={this.props.fetchUser}>获取数据</button>
             </div>
         )
     }
 }
 
-export default connect(null, { fetchUser })(User);
+const mapStateToProps = state => {
+    return {
+        user: state.user
+    };
+};
+
+export default connect(mapStateToProps, { fetchUser })(User);
--- src/sagas/user.js
+++ src/sagas/user.js
@@ -1,9 +1,14 @@
-import { takeEvery, call } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
 import axios from 'axios';
-import { FETCH_USER } from "../pages/user/store/actionTypes";
+import { FETCH_USER, FETCH_USER_SUCCESS, FETCH_USER_FAILURE } from "../pages/user/store/actionTypes";
 function* fetchUser() {
-    const user = yield call(axios.get, "https://api.github.com/users");
-    console.log(user);
+    try {
+        // https://api.github.com/users
+        const { data } = yield call(axios.get, "https://autumnfish.cn/api/joke/list?num=3");
+        yield put({ type: FETCH_USER_SUCCESS, payload: data });
+    } catch(e) {
+        yield put({ type: FETCH_USER_FAILURE, error: e.message });
+    }
 }
 function* watchFetchUser() {
     yield takeEvery(FETCH_USER, fetchUser);
