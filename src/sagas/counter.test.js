cd3b87d7a57f559e47bea829e9425bffde5fe595
ifer-itcast <yangkangkang@itcast.cn>
2020-08-17 18:01:45 +0800

    单元测试

---
+++ src/sagas/counter.test.js
@@ -0,0 +1,20 @@
+import test from 'tape';
+import { delay, put } from 'redux-saga/effects';
+import { incrementAsync } from './counter';
+import { increment } from '../pages/counter/store/actionCreators';
+
+test('incrementAsync saga test', function(assert) {
+    const it = incrementAsync();
+    // yield 什么，这里的 it.next().value 就是什么
+    assert.deepEqual(
+        it.next().value,
+        delay(2000),
+        "A promise with a delay of 2 s should be returned"
+    );
+    assert.deepEqual(
+        it.next().value,
+        put(increment()),
+        "An increase action should be initiated"
+    );
+    assert.end();
+});
